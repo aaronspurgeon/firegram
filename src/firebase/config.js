611210592419dbd0846134dd454e1f5a069ef442
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-17 17:06:09 -0400

    working upload to firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase";
+import "firebase/storage";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAmSDcrBFjrD-1_QjuXL5EE72qLxdzQHAI",
+  authDomain: "firegram-26e8c.firebaseapp.com",
+  databaseURL: "https://firegram-26e8c.firebaseio.com",
+  projectId: "firegram-26e8c",
+  storageBucket: "firegram-26e8c.appspot.com",
+  messagingSenderId: "763643766961",
+  appId: "1:763643766961:web:9caa1f183df202de9c1a90",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore, projectStorage };
