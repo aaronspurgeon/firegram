315895538689237e77d9781be5840a39f5791500
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-20 17:48:37 -0400

    working on column view

--- src/comps/ImageGrid.jsx
+++ src/comps/ImageGrid.jsx
@@ -1,16 +1,49 @@
 import React from 'react'
 import useFirestore from '../hooks/useFirestore';
 import { motion } from 'framer-motion';
+import { useState } from 'react';
+
+
+// { <div className="smooth-image-wrapper">
+// <img
+//   src={src}
+//   alt={alt}
+//   className={`smooth-image image-${
+//     imageLoaded ? 'visible' :  'hidden'
+//   }`}
+//   onLoad={()=> setImageLoaded(true)}}
+// />
+// {!imageLoaded && (
+//   <div className="smooth-preloader">
+//     <span className="loader" />
+//   </div>
+// )}
+// </div> */}
 
 export default function ImageGrid({ setSelectedImg }) {
     const { docs } = useFirestore('images')
+    const [imageLoaded, setImageLoaded] = useState(false)
     console.log(docs)
     return (
-        <div className='img-grid'>
+        <div className='img-col'>
             {docs && docs.map(doc => (
                 <div key={doc.id}>
-                    <motion.div className='img-wrap' onClick={() => setSelectedImg(doc.url)} whileHover={{ opacity: 1 }} layout>
-                        <motion.img src={doc.url} alt="firestore things" initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 1 }} />
+                    <motion.div className='img-wrap-col' onClick={() => setSelectedImg(doc.url)} whileHover={{ opacity: 1 }} layout>
+                        <motion.img src={doc.url}
+                            alt="firestore things"
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ delay: 1 }}
+                            className={`smooth-image image-${
+                                imageLoaded ? 'visible' : 'hidden'
+                                }`}
+                            onLoad={() => setImageLoaded(true)}
+                        />
+                        {!imageLoaded && (
+                            <div className="smooth-preloader">
+                                <span className="loader" />
+                            </div>
+                        )}
 
                     </motion.div>
                     <div style={{ textAlign: 'center' }}>
@@ -19,5 +52,6 @@ export default function ImageGrid({ setSelectedImg }) {
                 </div>
             ))}
         </div>
+
     )
 }
--- src/comps/NavBar.jsx
+++ src/comps/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { VscMail, VscHeart } from 'react-icons/vsc'
-import { AiOutlinePlus } from 'react-icons/ai'
+import { BsPlus } from 'react-icons/bs'
 import img from '../0.jpeg'
 
 export default function NavBar({ handleFormView }) {
@@ -10,7 +10,10 @@ export default function NavBar({ handleFormView }) {
         <div className="nav">
             <p className='empty'></p>
             <div className='items'>
-                <div onClick={handleFormView} className='btn' style={{ cursor: 'pointer' }}><AiOutlinePlus size='1.5em' /> <p>Create new post</p></div>
+                <div onClick={handleFormView} className='btn' style={{ cursor: 'pointer' }}>
+                    <BsPlus size='1.5em' />
+                    <p>Create new post</p>
+                </div>
                 <p style={{ cursor: 'pointer' }}><VscMail size='2em' /></p>
                 <p style={{ cursor: 'pointer' }}><VscHeart size='2em' /></p>
                 <img src={img} alt="my face" style={{ height: '50%', borderRadius: '50%', cursor: 'pointer' }} />
