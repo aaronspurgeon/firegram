88b584ef7a23037f5beea113f2c1d15b6de64edc
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-08-19 22:01:14 -0400

    added drop shadows to the images

--- src/comps/SideBar.jsx
+++ src/comps/SideBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import img from '../0.jpeg'
 import { BsHouseDoor, BsCompass } from 'react-icons/bs'
 import { HiOutlineHashtag } from 'react-icons/hi'
--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,8 +3,7 @@ import React from "react";
 const Title = () => {
   return (
     <div className="title">
-      <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <h2>Latest Feed</h2>
     </div>
   );
 };
